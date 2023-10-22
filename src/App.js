f7a5c9cd6f66a1d5cc8ba2b605ef7ddabf081746
shreyarchi <128414539+shreyarchi@users.noreply.github.com>
2023-10-22 23:12:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import AgeCalculator from "./component/AgeCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
